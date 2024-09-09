4865198510a1c7ef48dd8e400551c467d79a312d
Shireen <154674769+ShireenKachroo@users.noreply.github.com>
2024-09-09 17:15:51 +0530

    best time to buy/sell a stock

---
+++ Arrays/StockBuySell.cpp
@@ -0,0 +1,14 @@
+// if you are selling on the ith day, you can buy min price from 1st to (i-1) day;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int mini = prices[0];
+        for(int i=1;i<prices.size();i++){
+            int cost = prices[i] - mini;
+            profit = max(profit,cost);
+            mini = min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
