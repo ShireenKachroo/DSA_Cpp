9b0f87ca0c3291c89f650c7db8fe6987473ef7d0
Shireen <154674769+ShireenKachroo@users.noreply.github.com>
2024-09-19 19:06:05 +0530

    Update preorderTraversal.cpp

--- Binary Trees/preorderTraversal.cpp
+++ Binary Trees/preorderTraversal.cpp
@@ -1,8 +1,15 @@
-//PREORDER---> ROOT->LEFT->RIGHT
+//PREORDER---> ROOT->LEFT->RIGHT      TC: O(N) AND SC O(N)
 
-void preorder(Node* root){
-  if(root == NULL) return; //TC:O(N)  S.C:O(N)
-  cout<<root->data<<" "; 
-  preorder(root->left);
-  preorder(root->right);
-}
+vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>pre;
+        if(root == NULL) return pre;
+        //root
+        pre.push_back(root->val);
+        //left
+        vector<int>left = preorderTraversal(root->left);
+        pre.insert(pre.end(), left.begin(), left.end());
+        //right
+        vector<int>right = preorderTraversal(root->right);
+        pre.insert(pre.end(), right.begin(), right.end());
+        return pre;
+    }
