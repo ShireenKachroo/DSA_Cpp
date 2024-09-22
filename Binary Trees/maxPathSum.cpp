5aad607a4a6e953070dd7a4de49e876ae2516c25
Shireen <154674769+ShireenKachroo@users.noreply.github.com>
2024-09-22 13:17:34 +0530

    Create maxPathSum.cpp

---
+++ Binary Trees/maxPathSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxPath(TreeNode* node, int &maxi){
+        if(node == NULL) return 0;
+        int left = max(0, maxPath(node->left,maxi));
+        int right = max(0, maxPath(node->right,maxi));
+        maxi = max(maxi, left+right+node->val);
+        return (max(left,right)+node->val);
+    }
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPath(root,maxi);
+        return maxi;
+    }
+};
