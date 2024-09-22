73c4b43934eff91af15c385c13745ee44cafd53f
Shireen <154674769+ShireenKachroo@users.noreply.github.com>
2024-09-22 09:57:29 +0530

    Create preorder2stacks.cpp

---
+++ Binary Trees/preorder2stacks.cpp
@@ -0,0 +1,19 @@
+//preorder traversal using 2 stacks
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root == NULL) return ans;
+        stack<TreeNode*>s1,s2;
+        s1.push(root);
+        while(!s1.empty()){
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left != NULL) s1.push(root->left);
+            if(root->right != NULL) s1.push(root->right); 
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return ans;
+    }
