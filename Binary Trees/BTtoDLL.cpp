ebf2c58d7f8ea74cd189dbaabc611dad377452dd
Shireen <154674769+ShireenKachroo@users.noreply.github.com>
2024-10-20 12:20:06 +0530

    Create BTtoDLL.cpp

---
+++ Binary Trees/BTtoDLL.cpp
@@ -0,0 +1,30 @@
+/*convert a BT to inorder DLL*/
+
+//helper function
+void BTtoLL(Node* root, Node*&head, Node*&tail){
+  if(root == NULL) return;
+  
+  //creating DLL form left subtree
+  BTtoLL(root->left,head,tail);
+  
+  //connecting the current root
+  if(!head) head = root;
+  else{
+    root->left = tail;
+    tail->right = root;
+  }
+  
+  //creating DLL from right subtree
+  BTtoLL(root->right,head,tail);
+  
+  tail = root; //updating tail
+}
+
+//main function
+Node* convertedDLL(Node*root){
+  if(root == NULL) return nullptr;
+  Node* head = nullptr;
+  Node* tail = nullptr;
+  BTtoLL(root,head,tail);
+  return head; //head of new DLL
+}
